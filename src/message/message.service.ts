08e594539f6d5afca4ec8aeb2dc978039776fbb3
SeungHwan <silofn523@gmail.com>
2025-01-11 19:00:15 +0900

    Add files via upload

--- src/message/message.service.ts
+++ src/message/message.service.ts
@@ -1,49 +1,44 @@
 /* eslint-disable prettier/prettier */
-import { Injectable, InternalServerErrorException } from '@nestjs/common'
-import { SendMessageDto } from './dto/send-message.dto';
+import { Injectable, NotAcceptableException } from '@nestjs/common'
+import { SendMessageDto } from './dto/send-message.dto'
 
 @Injectable()
 export class MessageService {
-  
   public async sendMessageToMe(token: string, dto: SendMessageDto) {
-    try {
-      const { objType, text, webUrl, mobileUrl, btnTitle } = dto
+    const { objType, text, webUrl, mobileUrl, btnTitle } = dto
 
-      const response = await fetch('https://kapi.kakao.com/v2/api/talk/memo/default/send', {
-        method: 'POST',
-        headers: {
-          Authorization: `Bearer ${token}`,
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body: new URLSearchParams({
-          template_object: JSON.stringify({
-            object_type: objType,
-            text: text,
-            link: {
-              web_url: webUrl,
-              mobile_web_url: mobileUrl,
-            },
-            button_title: btnTitle
-          }),
-        }),
-      });
-  
-      const result = await response.json();
-  
-      if (response.ok) {
-        return {
-          success: true,
-          data: result,
-        };
-      } else {
-        throw new Error(`메시지 전송 에러 : ${result.msg}`);
+    const response = await fetch('https://kapi.kakao.com/v2/api/talk/memo/default/send', {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: new URLSearchParams({
+        template_object: JSON.stringify({
+          object_type: objType,
+          text: text,
+          link: {
+            web_url: webUrl,
+            mobile_web_url: mobileUrl
+          },
+          button_title: btnTitle
+        })
+      })
+    })
+
+    const result = await response.json()
+
+    if (response.ok) {
+      return {
+        success: true,
+        message: '메시지 전송 성공',
+        data: result
       }
-    } catch (error) {
-      throw new InternalServerErrorException({
+    } else {
+      throw new NotAcceptableException({
         success: false,
-        message: `메시지 전송 실패 : ${error.message}`,
-      });
+        message: `메시지 전송 에러 : ${result.msg}`
+      })
     }
   }
-  
 }
